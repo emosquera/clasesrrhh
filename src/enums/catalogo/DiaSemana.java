eb6c4ab5ce1139d354a4c4bf32ae2dff52352319
Oswaldo Babaresco <oswaldo_217@hotmail.com>
2018-02-18 11:22:51 -0400

    Se agregan las Clase: 	-TipoEventoResolucion Se agregan los Enumerados 	-DiaSemana 	-NaturalezaEvento

---
+++ src/enums/catalogo/DiaSemana.java
@@ -0,0 +1,22 @@
+package enums.catalogo;
+
+public enum DiaSemana {
+    LUNES       ("LU"),
+    MARTES      ("MA"),
+    MIERCOLES   ("MI"),
+    JUEVES      ("JU"),
+    VIERNES     ("VI"),
+    SABADO      ("SA"),
+    DOMINGO     ("DO");
+    
+        private final String diaSemana;
+        
+        private DiaSemana (String diaSemana) {
+		this.diaSemana = diaSemana;
+	}
+
+	public String getDiaSemana() {
+		return diaSemana;
+	}
+    
+}
